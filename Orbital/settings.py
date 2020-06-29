2445eaf4c775a6276b7a8884c5b0ad43f5590fa8
NUadS <65215643+NUadS@users.noreply.github.com>
2020-06-30 00:36:43 +0800

    Update settings.py

--- Orbital/settings.py
+++ Orbital/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= '/home/orbital/Orbital/static'
-STATICFILES_DIRS = [STATIC_DIR,]
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = '/accounts/user_login/'
